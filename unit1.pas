2efd61d5bbfa8aa5b0df0c5bae99e2fe618c4012
tomsik68 <tomsik68@gmail.com>
2015-03-10 08:33:41 +0100

    Pri kliknuti na tlacidlo sa prida riadok do mema

--- unit1.pas
+++ unit1.pas
@@ -13,6 +13,8 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -25,6 +27,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Memo1.Lines.Add('abcd');
+end;
 
 end.
 
